719ee31a2a42a8850b564943cb4954c1ade3478c
Luis Ceja <lcejaqui@gmail.com>
2026-09-21 22:05:53 -0700

    Fixed verify_input logic. Accidentally confused boolean 1/0 with error code 1/0, so changed from int to bool to prevent this logic bug.

--- 07-ChangeCalculator/changecalc.c
+++ 07-ChangeCalculator/changecalc.c
@@ -10,14 +10,15 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 void print_change(int big, int small);
-int verify_input(char *input, int *big, int *small);
-int is_numerical(char *str);
+bool verify_input(char *input, int *big, int *small);
+bool is_numerical(char *str);
 
 int main()
 {
-    char input[64];
+    char input[64] = {0};
     int big, small;
 
     while(1){
@@ -28,7 +29,7 @@ int main()
             break;
 
         if(!verify_input(input, &big, &small)){
-            printf("Bad input");
+            printf("Bad input\n");
             continue;
         }
         
@@ -37,39 +38,39 @@ int main()
     return 0;
 }
 
-int verify_input(char *input, int *big, int *small)
+bool verify_input(char *input, int *big, int *small)
 {
 	int count = 0;
 	char *str;
 	while((str = strtok(input, ".")) != NULL){
-		switch(count){
+		switch(count++){
 			case 0:
 				if(!is_numerical(str))
-					return 1;
+					return false;
 				*big = atoi(str);
 				break;
 			case 1:
 				if(!is_numerical(str))
-					return 1;
+					return false;
 				*small = atoi(str);
 				break;
 			default:
-				return 1;
+				return false;
 		}
 	}
-    return 0;
+    return true;
 }
 
-int is_numerical(char *str)
+bool is_numerical(char *str)
 {
 	int i = 0;
 	int c;
 	while((c = str[i]) != '\0'){
 		if(c < '0' || c > '9')
-			return 1;
+			return false;
 		i++;
 	}
-	return 0;
+	return true;
 }
 
 void print_change(int big, int small)
