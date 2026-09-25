b6233ce7de697bdf207bfb2ee9265d0f7a024f87
Luis Ceja <lcejaqui@gmail.com>
2026-09-25 12:05:31 -0700

    Started implementing median and mode.

--- 08-Mean,Median,andMode/3m.c
+++ 08-Mean,Median,andMode/3m.c
@@ -1,17 +1,28 @@
+#include <stdlib.h>
+#include <math.h>
+
 typedef struct{
     int count;
     int *list;
-}mode_t;
+}m_mode_t;
 
 typedef struct{
     int count;
     int list[2];
-}median_t;
+}m_median_t;
 
-void free_mode_t(mode_t *p){
+int __compare_int(const void *arg1, const void arg2){
+    int a = *(const int *)arg1;
+    int b = *(const int *)arg2;
+    if(a < b)
+        return -1;
+    if(a > b)
+        return 1;
+    return 0;
+}
+
+void free_mode_t(m_mode_t *p){
     if(p != NULL){
-        if(p->list != NULL)
-            free(p->list);
         free(p);
     }
 }
@@ -39,18 +50,35 @@ double mean(int count, int *list, int round){
     return (double)((int)(mean * p - 0.5)) / p; 
 }
 
-median_t *median(int count, int *list, int round){
-    double median;
+int median(m_median_t *median, int count, int *list){
+    if(!median)
+        return -1;
+
     // Sort list
-    
-    // Find mid point
-    
-    // Return both numbers if list is even
-    
+    qsort(list, count, sizeof(int), __compare_int);
+
+    if(count % 2 == 0){
+        median->count = 1;
+        median->list[0] = list[(int) count / 2];
+    }else{
+        median->count = 2;
+        median->list[1] = list[(int) count / 2];
+        median->list[0] = list[(int) (count / 2 - 1)];
+    }
+    return 0;
 }
 
-mode_t *mode(int count, int *list, int round){
-    int mode;
+int mode(m_mode_t *mode, int count, int *list){
 
-    return mode;
+    if(!mode)
+        return -1;
+
+    void *mem = malloc(count * sizeof(int[2]));
+
+    if(!mem)
+        return -1;
+
+    for(int = 0; i < count; i)
+
+    return 0;
 }
